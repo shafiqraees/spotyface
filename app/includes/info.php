320c0f3fb91d9d04a6eccccada24c3fb8d520e03
unknown <malikshafq7088@gmail.com>
2021-05-17 13:56:39 +0500

    all new changes done

--- app/includes/info.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define('SOFTWARE_NAME', 'Fir');
-define('SOFTWARE_AUTHOR', 'Fir');
-define('SOFTWARE_URL', 'http://codefir.com');
-define('SOFTWARE_VERSION', '1.0.0');
--- public/themes/boxplace/views/shared/token.php
+++
@@ -1,7 +0,0 @@
-<?php
-defined('FIR') OR exit();
-/**
- * The template for displaying the token input
- */
-?>
-<input type="hidden" name="token_id" value="<?=$data['token_id']?>">
